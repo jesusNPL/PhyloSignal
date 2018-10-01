59ee2ebfcdfbd2132f8acb42e4cad6ab0d01d752
Jesús N. Pinto-Ledezma <jesuspintoledezma@gmail.com>
2018-10-01 11:27:11 -0500

    Update PhyloSignal.R

--- PhyloSignal.R
+++ PhyloSignal.R
@@ -137,10 +137,10 @@ calcAICw <- function(tabla){
   if ( ! ("geiger" %in% installed.packages())) {install.packages("geiger", dependencies = T)}
   pp <- split(tabla, seq(nrow(tabla)))
   aicValues <- list()
-  for(i in 1:lenght(pp)){
+  for(i in 1:length(pp)){
   aicValues[[i]] <- geiger::aicw(as.numeric(pp[[i]]))
   }
-  Phylo <-  as.list(paste("Phylo", 1:lenght(pp), sep = "_"))
+  Phylo <-  as.list(paste("Phylo", 1:length(pp), sep = "_"))
   Model <- c("lambda", "BM", "noSignal")
   aicVALS <- do.call(rbind, aicValues)
   aicVALS$Phylo <- rep(Phylo, each = 3)
