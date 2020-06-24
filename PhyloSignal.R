fc5f5023757b7679491e0ad54919ffd96d356676
Jesús N. Pinto-Ledezma <jesuspintoledezma@gmail.com>
2020-06-23 19:13:28 -0500

    Update PhyloSignal.R

--- PhyloSignal.R
+++ PhyloSignal.R
@@ -140,20 +140,3 @@ K_model <- function(tree, trait, method = "K", test = TRUE, nsim = 999){
 
 #y <- K_model(tree = mbCCfull100, trait = sla, method = "K", test = TRUE, nsim = 9999)
 
-calcAICw <- function(tabla){
-  if ( ! ("geiger" %in% installed.packages())) {install.packages("geiger", dependencies = T)}
-  pp <- split(tabla, seq(nrow(tabla)))
-  aicValues <- list()
-  for(i in 1:length(pp)){
-  aicValues[[i]] <- geiger::aicw(as.numeric(pp[[i]]))
-  }
-  Phylo <-  as.list(paste("Phylo", 1:length(pp), sep = "_"))
-  Model <- c("lambda", "BM", "noSignal")
-  aicVALS <- do.call(rbind, aicValues)
-  aicVALS$Phylo <- rep(Phylo, each = 3)
-  aicVALS$Model <- rep(Model, each = 1)
-  names(aicVALS) <- c("AICc", "dAICc", "AICcw", "Phylogeny", "Model")
-  return(aicVALS)
-}
-
-#llll <- calcAICw(tabla = aicss)
